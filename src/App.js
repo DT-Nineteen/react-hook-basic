5878db807620e453d43b00987ebf7d56202ff31b
DT-Nineteeen <nick190901@gmail.com>
2022-07-04 21:33:39 +0700

    video #4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+let App = () => {
+  let name = 'Twilight'
+  let obj = { name: "Truong", age: 19 }
+  let link = 'https://www.facebook.com/Narutobu/'
   return (
     <div className="App">
       <header className="App-header">
+        {console.log("check obj", obj)}
+
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        {JSON.stringify(obj)}
+        <h1>Hello world with {name} in {obj.age}!</h1>
+        <a href={link} target='_blank'>Visit my profile</a>
       </header>
     </div>
   );
